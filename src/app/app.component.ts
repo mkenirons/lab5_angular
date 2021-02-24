255f37b9b9c26d64a38b28c491961b9901310e26
mkenirons <martin.kenirons@gmail.com>
2021-02-24 15:17:08 +0000

    Lab complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-first-app';
+
+  Name: string = "Martin";
+  Age: number = 22;
+
 }
